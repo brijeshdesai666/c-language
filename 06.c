54eb0688885e12173248c7b384f7801fae1b218f
brijesh desai <110248235+brijeshdesai666@users.noreply.github.com>
2022-07-29 22:13:58 +0530

    Update 06.c

--- 06.c
+++ 06.c
@@ -2,7 +2,7 @@
  
    int main() {
                 int g;
-                printf("*****Enter the number to choice multication table*****\n");
+                printf("******Enter the number to choice multication table******\n");
             scanf("%d", &g);
         for(int m = 1; m <= 10 ; m++) {
             printf(" %d X %d = %d\n", g,m ,m*g);
